e28b503148140be63c3e59dac52419ed3ec9e4bd
seojuni04 <seojuni.kim@gmail.com>
2017-07-31 09:59:02 -0700

    Delete Code and Renew Get My MAC,IP

--- main.c
+++ main.c
@@ -1,122 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pcap.h>
-#include <net/ethernet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 #include <netinet/in.h>
-#include <netinet/ether.h>
 #include <string.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-#include <unistd.h>
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/ether.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
+#include <net/ethernet.h>
 
-#define HAL 6
-#define PAL 4
-
-void get_mac_addr(struct ethhdr *ethh, char *interface){
-    int sock = socket(PF_INET, SOCK_DGRAM, 0);
-    struct ifreq req;
-    char *intface = interface;
-
-    if (sock < 0) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    memset(&req, 0, sizeof(req));
-    strncpy(req.ifr_name, intface, IF_NAMESIZE - 1);
-
-    if (ioctl(sock, SIOCGIFHWADDR, &req) < 0){
-        perror("ioctl");
-        exit(EXIT_FAILURE);
-    }
-
-    for(int i=0; i<6; i++)
-    {
-        ethh->h_source[i] = req.ifr_hwaddr.sa_data[i];
-        ethh->h_dest[i] = 255;
-    }
-
-    close(sock);
-}
-
-void make_eth_header(char *interface, char *sender_ip, struct ethhdr *ethh){
-    ethh->h_proto = htons(ETHERTYPE_ARP);
-    get_mac_addr(ethh, interface);
-    printf("dest mac : %s\n", ether_ntoa((struct ether_addr *)ethh->h_dest));
-    printf("source mac : %s\n", ether_ntoa((struct ether_addr *)ethh->h_source));
-}
-
-void make_arp_header(char *sender_ip, char *target_ip, struct ethhdr *ethh, struct ether_arp *arph){
-    arph->ea_hdr.ar_hrd = htons(ARPHRD_ETHER);
-    arph->ea_hdr.ar_pro = htons(ETH_P_IP);
-    arph->ea_hdr.ar_hln = HAL;
-    arph->ea_hdr.ar_pln = PAL;
-    arph->ea_hdr.ar_op = htons(ARPOP_REQUEST);
-
-    sscanf(sender_ip, "%d.%d.%d.%d", arph->arp_spa, arph->arp_spa+1, arph->arp_spa+2, arph->arp_spa+3);
-    sscanf(target_ip, "%d.%d.%d.%d", arph->arp_tpa, arph->arp_tpa+1, arph->arp_tpa+2, arph->arp_tpa+3);
-    for(int i=0; i<6; i++){
-        arph->arp_sha[i] = ethh->h_source[i];
-        arph->arp_tha[i] = 0;
-    }
-}
+struct	ETHER_ARP {
+    struct	arphdr ea_hdr;		/* fixed-size header */
+    u_int8_t arp_sha[ETH_ALEN];	/* sender hardware address */
+    u_int8_t *arp_spa;		/* sender protocol address */
+    u_int8_t arp_tha[ETH_ALEN];	/* target hardware address */
+    u_int8_t *arp_tpa;		/* target protocol address */
+};
 
 int main(int argc, char *argv[])
 {
-    pcap_t *handle;         /* Session handle */
-    char *interface;         /* The device to sniff on */
-    char errbuf[PCAP_ERRBUF_SIZE];   /* Error string */
-    bpf_u_int32 mask;      /* Our netmask */
-    bpf_u_int32 net;      /* Our IP */
-    const u_char *packet;      /* The actual packet */
-    struct ether_header *ethh;
-    struct ether_arp *arph;
-    u_char arp_packet[42];
+    char *dev;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    char *sender_ip, *target_ip;
+    struct ifreq ifreq_ip, ifreq_mac;
+    unsigned char *attacker_mac;
+    unsigned char *attacker_ip;
+    pcap_t *handle;
 
-    if (argc < 4) {
-        fprintf(stderr, "Usage: %s <interface> <sender ip> <target ip>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    if(argc<4)
+    {
+        printf("Usage: %s <interface> <sender ip> <target ip>\n", argv[0]);
+        exit(1);
     }
 
-    /* Define the device */
-    interface = argv[1];
+    dev = argv[1];
+    sender_ip = argv[2];
+    target_ip = argv[3];
 
-    /* Find the properties for the device */
-    if (pcap_lookupnet(interface, &net, &mask, errbuf) == -1) {
-        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", interface, errbuf);
-        net = 0;
-        mask = 0;
+    /* Open pcap */
+    handle = pcap_open_live(dev, BUFSIZ,1, 1000, errbuf);
+    if(handle == NULL)
+    {
+        fprintf(stderr, "Couldn't open device %s : %s\n", dev, errbuf);
+        return 2;
     }
 
-    /* Open the session in promiscuous mode */
-    handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
-
-    if (handle == NULL) {
-        fprintf(stderr, "Couldn't open device %s: %s\n", interface, errbuf);
-        return(2);
+    /* Get My(Attacker) MAC address */
+    int s = socket(AF_INET, SOCK_DGRAM,0);
+    if(s<0){
+        perror("socket fail");
     }
+    strncpy(ifreq_mac.ifr_name, dev, IFNAMSIZ);
+    if(ioctl(s, SIOCGIFHWADDR, &ifreq_mac)<0){
+        perror("ioctl fail");
+    }
+    attacker_mac = ifreq_mac.ifr_hwaddr.sa_data;
+    printf("My(Attacker) MAC address : %02x:%02x:%02x:%02x:%02x:%02x\n", attacker_mac[0], attacker_mac[1], attacker_mac[2], attacker_mac[3], attacker_mac[4], attacker_mac[5]);
 
-    /* Make Ethernet Header */
-    make_eth_header(interface, argv[2], &ethh);
+    /* Get My(Attacker) IP address */
+    struct sockaddr_in *sin;
+    strncpy(ifreq_ip.ifr_name, dev, IFNAMSIZ);
+    if(ioctl(s, SIOCGIFADDR, &ifreq_ip)<0) perror("ioctl fail");
+    sin = (struct sockaddr_in*)&ifreq_ip.ifr_addr;
+    attacker_ip=inet_ntoa(sin->sin_addr);
+    printf("attacker's IP address : %s\n", attacker_ip);
 
-    /* Make ARP Header */
-    make_arp_header(argv[2], argv[3], &ethh, &arph);
-    printf("1\n");
-    /* Send Packet */
-    memset(0xff, )
-    memcpy(arp_packet, &ethh, 14);
-    printf("2\n");
-    memcpy(arp_packet+14, &arph, 28);
-    printf("3\n");
-    printf("%d\n", sizeof(arp_packet));
-    pcap_sendpacket(handle, arp_packet, sizeof(arp_packet));
-
+    /* Close pcap */
+    pcap_close(handle);
     return 0;
 }
